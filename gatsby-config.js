9bc56979c5919402b99ca706b201a395f44563c1
Donfred <donfredevelopper@gmail.com>
2021-01-22 09:08:09 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  siteMetadata: {
+    title: "brikayiti-site-gatsby",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-134593991-1",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-offline",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
